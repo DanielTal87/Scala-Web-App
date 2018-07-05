79e537e4552c39203f36475eed9b22f0598f20b9
hadasB3 <hadas.barel3@gmail.com>
2018-07-05 12:31:30 +0300

    fix error

--- app/controllers/CouponController.scala
+++ app/controllers/CouponController.scala
@@ -19,7 +19,7 @@ class CouponController @Inject()(cc: ControllerComponents) extends AbstractContr
     */
   def show = Action { implicit request =>
     val mongo = new MongoDB()
-    if (mongo.isDBEmpty) {sdasd
+    if (mongo.isDBEmpty) {
       mongo.addCoupons()
     }
     val couponsMatrix: Array[Array[String]] = mongo.findAllCoupons
